013bccf7309bb721432685a7124b92c63d498c53
Yamuna Veeranna <M1030317@mindtree.com>
2017-04-14 13:15:04 +0530

    my first commit

---
+++ app3.js
@@ -0,0 +1,18 @@
+var exp = require("express");
+
+var app = exp();
+
+app.use(exp.static(__dirname + "/public"));
+
+app.get("/players/:name/:lang", function(req, res){
+	//console.log("query is here" +JSON.stringify(query));
+	//var query = req.query;
+	res.write("name=" + req.params.name);
+	res.write("  lang=" + req.params.lang);
+	res.end(JSON.stringify(req.params));
+});
+var port = process.env.PORT || 4000;
+
+app.listen(port, function(){
+	console.log("port is running on 4000");
+});
